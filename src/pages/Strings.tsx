a7ce26eca0509f7ad49e2fcd923c5a5b15a2f1cc
OhBeomho <tyler0322@naver.com>
2023-08-27 09:31:07 +0900

    making a page that genrates random strings

--- src/pages/Strings.tsx
+++ src/pages/Strings.tsx
@@ -1,5 +1,53 @@
+import { useCallback, useReducer, ChangeEvent } from "react";
 import Layout from "../components/Layout";
 
+interface State {
+	uppercases: boolean;
+	lowercases: boolean;
+	numbers: boolean;
+	length: number;
+	count: number;
+}
+
+interface Action {
+	type: "uppercases" | "lowercases" | "numbers" | "length" | "count";
+	payload: number | boolean;
+}
+
+function reducer(state: State, action: Action): State {
+	const { type, payload } = action;
+
+	switch (type) {
+		case "length":
+			return { ...state, length: Number(payload) };
+		case "count":
+			return { ...state, count: Number(payload) };
+		case "uppercases":
+		case "lowercases":
+		case "numbers":
+			return { ...state, [type]: Boolean(payload) };
+	}
+}
+
 export default function () {
-	return <Layout></Layout>;
+	const [state, dispatch] = useReducer(reducer, {
+		uppercases: false,
+		lowercases: true,
+		numbers: true,
+		length: 10,
+		count: 5
+	});
+	const changeState = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+		// TODO: Change state
+	}, []);
+	const generateStrings = useCallback(() => {
+		// TODO: Generate random strings
+	}, [state]);
+
+	// TODO: Add elements
+	return (
+		<Layout>
+			<h1>무작위 문자열 생성</h1>
+		</Layout>
+	);
 }
